35d0da1e23eb8e4cd76a850946c54fa96005e9c7
jessicamh13 <jessica.mh13@yahoo.com>
2018-07-10 15:39:53 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,15 +1,15 @@
 
 # Code your methods below
 
-def Stance
+def assume_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
 End 
-def Base
+def base_footwork
   puts "Lift right foot"
   puts "Return right foot to the ground"
   puts "Finishing with a small skip-step backward"
 End
-def Skip
+def skip_step
   puts "Lower left foot to the ground"
   puts "Bounce left foot back up slightly, kicking it a few inches back"
   puts "Left, Right, Left and Left"
